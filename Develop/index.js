5898da3eee81c9171c08d89bf168af60e4ffe5a5
aaron weiner <aaronjacobweiner@yahoo.com>
2021-03-22 10:41:20 -0400

    some chnages

--- Develop/index.js
+++ Develop/index.js
@@ -1,53 +1,73 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
 
 const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        message: 'What is your name?',
-        name: 'name',
+        message: 'What is your projects name?',
+        name: 'title',
     },
     {
         type: 'input',
-        message: 'What city do you live in?',
-        name: 'city',
+        message: 'Write a description.',
+        name: 'description',
+    },    
+    {
+        type: 'checkbox',
+        message: 'Install npm i',
+        name: 'npmi',
+        choices: 'npm i',
     },
     {
         type: 'input',
-        message: 'What is your favorite food?',
-        name: 'food',
+        message: 'Usage?',
+        name: 'usage',
     },
     {
         type: 'input',
-        message: 'What is your name?',
-        name: 'name',
+        message: 'What license is this project under?',
+        name: 'license',
     },
     {
         type: 'input',
-        message: 'What city do you live in?',
-        name: 'city',
+        message: 'What contributions were made?',
+        name: 'contributions',
+    },
+    {
+        type: 'prompt',
+        message: 'To run tests, enter the following command',
+        name: 'npm test',
     },
     {
         type: 'input',
-        message: 'What is your favorite food?',
-        name: 'food',
+        message: 'What is your email?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
     }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
 
-    fs.writeFile('README.md', createHtml(response), (err) =>
+    fs.writeFile('README.md', generateMarkdown(fileName, data), (err) =>
             err ? console.error(err) : console.log('Success!')
         );
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    writeToFile();
+    inquirer
+        .prompt(questions)
+        .then((response) =>
+            writeToFile(response)
+        )
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,52 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown({ title, description, email, username }) {
+  return `# ${title}  
+  [![License: ${renderLicenseSection()}](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)
+
+## Description
+${description}
+
+### Table of contents
+* Installation
+* Usage
+* License
+* Contributing
+* Tests
+* Questions
+
+
+### Installation
+##### To install necessary dependencies, run the following command:
+  ${npmi}
+    
+### Usage
+
+### License
+
+### Contributing
+
+### Tests
+
+### Questions
+##### If you have any questions about the repo, open an issue or contact me directly at ${email}. You can find more of my work at ${username}
 
 `;
 }
